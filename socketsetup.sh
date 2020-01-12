15238ba36afb2bc64ed1ee47fd3a1d388c3de585
greenfan <rdwyer@fastmail.com>
2020-01-12 14:42:14 -0800

    Create socketsetup.sh

---
+++ socketsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#to use this script properly, schedule it in cron, and setup ssh public key authentication
+#count number of ssh reverse tunnels running
+pids=$(pgrep ssh -a | grep R | awk '{print $1}')
+nums=$(pgrep ssh -a | grep R | awk '{print $1}'| wc -w)
+if [ "$nums" -ne 0 ]
+        then
+                if [ "$nums" -ge "3" ]
+                then kill -9 "$pids"
+                elif [ "$nums" -eq "2" ]
+                then exit 0
+                fi
+fi
+screen -dm ssh -o ServerAliveInterval=60 -R 7001:localhost:22 -p 1234 jumpacct@yourbastionhost.com
