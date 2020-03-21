5c0fa444f9ddd19ed27a3df813ecdb13a4db1776
greenfan <rdwyer@fastmail.com>
2020-03-21 15:12:04 -0700

    Update vectra_detect_ips_parser.sh

--- vectra_detect_ips_parser.sh
+++ vectra_detect_ips_parser.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
-hostcount=0
-for i in `cat $1`
-        do  count=$(cat $2 | sort | grep "$i" | wc -l)
-#       printf '%12s0/24 %-12s  %-10s\n' "$i" ""  "$count"
-        echo ""$i"0,"$count""
-        hostcount=$( expr $hostcount + $count )
+  
+hosts=detect_ips.csv
+subnets=/tmp/detected_subnets.txt
+uniq_nets=/tmp/uniq_subnets.txt
+for i in `cat $hosts`
+        do
+#strip last octet, and escape the period with a trailing backslash
+        subnet=$( echo "$i" | sed 's/\.[0-9]*$/\\./')
+        echo "$subnet" >> /tmp/detected_subnets.txt
         done
 
-echo "Total Active Hosts: $hostcount"
+cat "$subnets" | sort | uniq > "$uniq_nets"
+
+for i in `cat $uniq_nets`
+        do
+#strip off the trailing backslash
+        subnet=$(echo "$i" | sed 's/\\/0/g')
+
+#parse through the entire hosts file, count how many hosts are in each unique subnet, and output to a csv
+        sub_host_count=$(cat "$hosts" | grep "$i" | wc -l)
+        echo ""$subnet","$sub_host_count""
+
+done
+
+#compare yesterday's CSV with todays.
+#output all new hosts.
+#output all removed hosts.
+
+
+#automate vectra detect_ips.csv Importation!!!
+
+#cleanup tmp #files
+#rm -rf "$hosts" "$subnets" "$uniq_nets
